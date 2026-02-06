84b199c46050cdc794899aa9ca67750cb25e7061
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-02-06 21:24:17 +0600

    go

---
+++ bin/13-jhjvjhb.dart
@@ -0,0 +1,26 @@
+/**void main() {
+  int result = factorial(6);
+  print(result);
+}
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+**/
+
+void main() {
+  int result = factorial(6);
+  print(result);
+}
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
---
+++ bin/14.1-exception_handeling.dart
@@ -0,0 +1,8 @@
+void main() {
+  int x = 20;
+  int y = 0;
+
+  int result;
+  result = x ~/ y;
+  print(result);
+}
